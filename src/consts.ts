9199a37d3920b662f649b800caecd8f298559462
jordan <sudo.sendmail.jordan@gmail.com>
2025-09-23 19:27:20 -0700

    update README.md / update url

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_URL = "https://miniblog.nicholasly.com";
+export const SITE_URL = "https://github.com/sudo-adduser-jordan.github.io";
 export const SITE_TITLE = "Miniblog";
 export const SITE_DESCRIPTION =
   "Miniblog is an opinionated and extremely minimal blogging template built with Astro and Tailwind CSS.";
