811ede81613989d312d759a144f333c6096d6213
jordan <sudo.sendmail.jordan@gmail.com>
2025-09-23 19:46:31 -0700

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_URL = "https://github.com/sudo-adduser-jordan.github.io";
-export const SITE_TITLE = "Miniblog";
+export const SITE_TITLE = "Jordan";
 export const SITE_DESCRIPTION =
   "Miniblog is an opinionated and extremely minimal blogging template built with Astro and Tailwind CSS.";
 
