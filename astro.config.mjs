d7316167aedbef9ebc4c96b70d32c3d5e5f24a83
jordan <sudo.sendmail.jordan@gmail.com>
2025-09-24 17:53:31 -0700

    update packages / add expressive code / update post

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,16 +7,20 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { SITE_URL } from "./src/consts";
 
+import expressiveCode from "astro-expressive-code";
+
 // https://astro.build/config
 export default defineConfig({
   site: SITE_URL,
-  integrations: [mdx(), sitemap(), tailwind()],
-  markdown: {
-    shikiConfig: {
-      themes: {
-        light: "catppuccin-latte",
-        dark: "catppuccin-mocha",
+  integrations: [
+    expressiveCode({
+      themes: ['catppuccin-macchiato', 'catppuccin-latte'],
+      
+      defaultProps: {
+        wrap: true,
+        overridesByLang: { 'bash,ps,sh': { frame: 'none' }, },
       },
-    },
-  },
+    }), 
+    mdx(), sitemap(), tailwind()
+  ],
 });
