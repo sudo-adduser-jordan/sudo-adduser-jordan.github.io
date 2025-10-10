f23c01675ef9a267dd7aa46c12ad723c175462fc
jordan <sudo.sendmail.jordan@gmail.com>
2025-10-10 13:30:36 -0700

    add icons

--- src/consts.ts
+++ src/consts.ts
@@ -1,9 +1,4 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE_URL = "https://github.com/sudo-adduser-jordan.github.io";
 export const SITE_TITLE = "Jordan";
-export const SITE_DESCRIPTION =
-  "Jordan, Software Engineer";
-
+export const SITE_DESCRIPTION ="Jordan, Software Engineer";
 export const EMAIL = "sudo.sendmail.jordan@gmail.com";
